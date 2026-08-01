cc61ce4f126bd3492fd3a8fe44948ecbc92d535f
Cameron Davies <cameron@tonitum.com>
2026-07-31 18:56:46 -0600

    fix: register delete api

--- cmd/core-service/main.go
+++ cmd/core-service/main.go
@@ -12,5 +12,6 @@ func main() {
 	http.HandleFunc("POST /new", core.RegisterEntry)
 	http.HandleFunc("GET /{alias}", core.LoadEntry)
 	http.HandleFunc("GET /list", core.ListURLS)
+	http.HandleFunc("DELETE /{alias}", core.DeleteEntry)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
